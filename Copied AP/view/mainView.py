60b5b4ce54c4c91b5f7dd13a71704f48071fa245
Anshul Pandey <anshulpandey@Anshuls-MacBook-Air.local>
2025-06-08 13:36:56 +0200

    Added widgets to every layout and changed the code to make it easy to understand

--- Copied AP/view/mainView.py
+++ Copied AP/view/mainView.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QRadioButton, QPushButton, QSpacerItem, QSizePolicy, QCheckBox
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QRadioButton, QPushButton, \
+    QSpacerItem, QSizePolicy, QCheckBox
 from PyQt5.QtCore import Qt
 from .plotView import VisPyPlotWidget
+
 
 class MainView(QMainWindow):
     """
@@ -30,90 +32,101 @@ class MainView(QMainWindow):
         # Set up the main window
         self.setWindowTitle("VeryCreativeProjectName")
         self.setGeometry(0, 0, 800, 600)
-        #self.showFullScreen()  # Enters full screen mode
+        self.showFullScreen()  # Enters full screen mode
 
         # Create central widget and layout
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        ##Layout Initializations
-        main_vertical_layout = QVBoxLayout(central_widget)
-        horizontal_layout = QHBoxLayout()
-        control_centre = QVBoxLayout()
-        imp_buttons = QHBoxLayout()
-        main_button = QVBoxLayout()
-        check_group = QHBoxLayout()
-        check_columns_1 = QVBoxLayout()
-        check_columns_2 = QVBoxLayout()
-        check_columns_3 = QVBoxLayout()
-        check_columns_4 = QVBoxLayout()
+        main_vertical_layout = QVBoxLayout()
+        central_widget.setLayout(main_vertical_layout)
+
+        top_bar_widget = QWidget()
         top_bar = QHBoxLayout()
+        top_bar_widget.setLayout(top_bar)
+        top_bar_widget.setFixedHeight(150)
 
-        ##Widget Initializations
-        self.plot_widget = VisPyPlotWidget()
-        button_rms = QPushButton("RMS Signal")
-        button_raw = QPushButton("Raw Signal")
-        button_filt = QPushButton("Filtered Signal")
-        select_channels_label = QLabel("Select Channels")
-        size_widget = QWidget()
-        size_widget.setLayout(top_bar)
-        size_widget.setFixedHeight(100)
+        spacer = QSpacerItem(1100, 100, QSizePolicy.Minimum, QSizePolicy.Fixed)
+        top_bar.addSpacerItem(spacer)
 
-        check_list = []
-        for i in range(1, 33, 1):
-            check_list.append(QCheckBox(str(i)))
+        main_button_widget = QWidget()
+        main_button = QVBoxLayout()
+        main_button_widget.setLayout(main_button)
 
-        all = QPushButton("All")
-        none = QPushButton("None")
         on_off_butt = QPushButton("MAIN SWITCH")
         status_txt = QLabel("Status: CONNECTED")
         ip_txt = QLabel("IP: 62.214.70.46")
-
-        spacer = QSpacerItem(1100,100, QSizePolicy.Minimum, QSizePolicy.Fixed)
-        top_bar.addSpacerItem(spacer)
-
-
-        main_vertical_layout.addWidget(size_widget)
-        main_vertical_layout.addLayout(horizontal_layout)
-        main_vertical_layout.addWidget(self.plot_widget)
-        horizontal_layout.addLayout(control_centre)
-        horizontal_layout.addWidget(self.plot_widget)
-
-        top_bar.addLayout(main_button)
-
 
         main_button.addWidget(on_off_butt)
         main_button.addWidget(status_txt)
         main_button.addWidget(ip_txt)
 
+        top_bar.addWidget(main_button_widget)
+
+        main_vertical_layout.addWidget(top_bar_widget)
+
+        horizontal_layout = QHBoxLayout()
+        horizontal_layout_widget = QWidget()
+        horizontal_layout_widget.setLayout(horizontal_layout)
+
+        control_centre = QVBoxLayout()
+        control_centre_widget = QWidget()
+        control_centre_widget.setLayout(control_centre)
+
+        button_rms = QPushButton("RMS Signal")
+        button_raw = QPushButton("Raw Signal")
+        button_filt = QPushButton("Filtered Signal")
+        select_channels_label = QLabel("Select Channels")
+
         control_centre.addWidget(button_raw)
         control_centre.addWidget(button_filt)
         control_centre.addWidget(button_rms)
         control_centre.addWidget(select_channels_label)
-        control_centre.addLayout(imp_buttons)
-        control_centre.addLayout(check_group)
+
+        imp_buttons = QVBoxLayout()
+        imp_buttons_widget = QWidget()
+        imp_buttons_widget.setLayout(imp_buttons)
+
+        all = QPushButton("All")
+        none = QPushButton("None")
 
         imp_buttons.addWidget(all)
         imp_buttons.addWidget(none)
 
-        check_group.addLayout(check_columns_1)
-        check_group.addLayout(check_columns_2)
-        check_group.addLayout(check_columns_3)
-        check_group.addLayout(check_columns_4)
+        control_centre.addWidget(imp_buttons_widget)
 
-        for x in range(int(len(check_list) / 4)):
+        check_group = QHBoxLayout()
+        check_group_widget = QWidget()
+        check_group_widget.setLayout(check_group)
+
+        check_columns_1 = QVBoxLayout()
+        check_columns_1_widget = QWidget()
+        check_columns_1_widget.setLayout(check_columns_1)
+
+        check_columns_2 = QVBoxLayout()
+        check_columns_2_widget = QWidget()
+        check_columns_2_widget.setLayout(check_columns_2)
+
+        check_list = []
+        for i in range(1, 33, 1):
+            check_list.append(QCheckBox(str(i)))
+
+        for x in range(int(len(check_list) / 2)):
             check_columns_1.addWidget(check_list[x])
 
-        for y in range(int(len(check_list) / 4), int(len(check_list) / 2)):
+        for y in range(int(len(check_list) / 2), int(len(check_list))):
             check_columns_2.addWidget(check_list[y])
 
-        for z in range(int(len(check_list) / 2), int(len(check_list) * 3 / 4)):
-            check_columns_3.addWidget(check_list[z])
+        check_group.addWidget(check_columns_1_widget)
+        check_group.addWidget(check_columns_2_widget)
+        control_centre.addWidget(check_group_widget)
 
-        for t in range(int(len(check_list) * 3 / 4), int(len(check_list))):
-            check_columns_4.addWidget(check_list[t])
+        horizontal_layout.addWidget(control_centre_widget)
+        self.plot_widget = VisPyPlotWidget()
+        horizontal_layout.addWidget(self.plot_widget)
 
-        # Create control button
+        main_vertical_layout.addWidget(horizontal_layout_widget)
+
         self.control_button = QPushButton("Start Plotting")
         self.control_button.clicked.connect(self.toggle_plotting)
         main_vertical_layout.addWidget(self.control_button)
@@ -130,6 +143,7 @@ class MainView(QMainWindow):
         else:
             self.control_button.setText("Stop Plotting")
             self.view_model.start_plotting()
+
 
 
 
