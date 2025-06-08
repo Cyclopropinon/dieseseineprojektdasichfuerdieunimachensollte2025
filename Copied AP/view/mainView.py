3268f37a150a2f6b1cb5999cf805f024ec6c87f0
Anshul Pandey <anshulpandey@Anshuls-MacBook-Air.local>
2025-06-08 16:59:50 +0200

    Changed orientation of buttons and aligned them in the center

--- Copied AP/view/mainView.py
+++ Copied AP/view/mainView.py
@@ -39,7 +39,7 @@ class MainView(QMainWindow):
         self.setCentralWidget(central_widget)
 
         ## sets a kuhl border so we can see the dingsdas
-        central_widget.setStyleSheet("border: 1px solid red;")
+        #central_widget.setStyleSheet("border: 1px solid red;")
 
         main_vertical_layout = QVBoxLayout()
         central_widget.setLayout(main_vertical_layout)
@@ -59,37 +59,36 @@ class MainView(QMainWindow):
 
         on_off_butt = QPushButton("MAIN SWITCH")
         on_off_butt.setFixedSize(100, 100);
-        on_off_butt.setStyleSheet("border-radius: 50%; background-color: lightgreen;")
+        on_off_butt.setStyleSheet("border-radius: 50%; background-color: lightgreen; border: 5px solid darkgreen;")
         status_txt = QLabel("Status: CONNECTED")
-        ip_txt = QLabel("IP: 62.214.70.46")
+        ip_txt = QLabel("IP: 62.214.70.46:8080")
 
         main_button.addWidget(on_off_butt)
         main_button_txt = QVBoxLayout()
-        main_button_txt_widget = QWidget()
-        main_button_txt_widget.setLayout(main_button_txt)
+
 
         main_button_txt.addWidget(status_txt)
         main_button_txt.addWidget(ip_txt)
 
 
-        main_button.addWidget(main_button_txt_widget)
+        main_button.addLayout(main_button_txt)
 
         main_vertical_layout.addWidget(top_bar_widget)
 
         top_bar.addWidget(main_button_widget)
 
         horizontal_layout = QHBoxLayout()
-        horizontal_layout_widget = QWidget()
-        horizontal_layout_widget.setLayout(horizontal_layout)
 
         control_centre = QVBoxLayout()
         control_centre_widget = QWidget()
         control_centre_widget.setLayout(control_centre)
+        control_centre_widget.setFixedWidth(250)
 
         button_rms = QPushButton("RMS Signal")
         button_raw = QPushButton("Raw Signal")
         button_filt = QPushButton("Filtered Signal")
         select_channels_label = QLabel("Select Channels")
+        select_channels_label.setAlignment(Qt.AlignCenter)
 
         control_centre.addWidget(button_raw)
         control_centre.addWidget(button_filt)
@@ -97,28 +96,32 @@ class MainView(QMainWindow):
         control_centre.addWidget(select_channels_label)
 
         imp_buttons = QHBoxLayout()
-        imp_buttons_widget = QWidget()
-        imp_buttons_widget.setLayout(imp_buttons)
 
         all = QPushButton("All")
+        all.setFixedSize(60, 40)
+        all.setStyleSheet("background-color: darkgrey; border: 2px solid black;")
         none = QPushButton("None")
+        none.setFixedSize(60, 40)
+        none.setStyleSheet("background-color: darkgrey; border: 2px solid black;")
 
         imp_buttons.addWidget(all)
         imp_buttons.addWidget(none)
 
-        control_centre.addWidget(imp_buttons_widget)
+        control_centre.addLayout(imp_buttons)
 
         check_group = QHBoxLayout()
-        check_group_widget = QWidget()
-        check_group_widget.setLayout(check_group)
 
         check_columns_1 = QVBoxLayout()
         check_columns_1_widget = QWidget()
         check_columns_1_widget.setLayout(check_columns_1)
+        check_columns_1_widget.setFixedWidth(50)
 
         check_columns_2 = QVBoxLayout()
         check_columns_2_widget = QWidget()
         check_columns_2_widget.setLayout(check_columns_2)
+        check_columns_2_widget.setFixedWidth(50)
+
+
 
         check_list = []
         for i in range(1, 33, 1):
@@ -127,18 +130,19 @@ class MainView(QMainWindow):
         for x in range(int(len(check_list) / 2)):
             check_columns_1.addWidget(check_list[x])
 
+
         for y in range(int(len(check_list) / 2), int(len(check_list))):
             check_columns_2.addWidget(check_list[y])
 
         check_group.addWidget(check_columns_1_widget)
         check_group.addWidget(check_columns_2_widget)
-        control_centre.addWidget(check_group_widget)
+        control_centre.addLayout(check_group)
 
         horizontal_layout.addWidget(control_centre_widget)
         self.plot_widget = VisPyPlotWidget()
         horizontal_layout.addWidget(self.plot_widget)
 
-        main_vertical_layout.addWidget(horizontal_layout_widget)
+        main_vertical_layout.addLayout(horizontal_layout)
 
         self.control_button = QPushButton("Start Plotting")
         self.control_button.clicked.connect(self.toggle_plotting)
